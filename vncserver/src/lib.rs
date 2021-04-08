710dc6411ee1613967f6bd450329c51448109bde
rayylee <rayylee@foxmail.com>
2021-04-08 15:44:15 +0800

    vncserver: add rfb mod

--- vncserver/src/lib.rs
+++ vncserver/src/lib.rs
@@ -1,81 +1,32 @@
-#![allow(improper_ctypes)]
-#![allow(non_snake_case)]
-#![allow(non_upper_case_globals)]
-#![allow(non_camel_case_types)]
+mod rfb;
 
-//! # Libvncserver bindings
-//!
-//! This library contains safe Rust bindings for [Libvncserver](https://github.com/LibVNC/libvncserver)
-//!
-//! # Sample
-//!
-//! ```no_run
-//! use vncserver::*;
-//! fn main() {
-//!     let server = rfb_get_screen(400, 300, 8, 3, 4);
-//!     rfb_framebuffer_malloc(server, 400*300*4);
-//!     rfb_init_server(server);
-//!     rfb_run_event_loop(server, -1, 0);
-//! }
-//! ```
-
-use std;
-use core::slice;
-
-include!(concat!(std::env!("OUT_DIR"), "/rfb.rs"));
-
-
-pub type RfbScreenInfoPtr = rfbScreenInfoPtr;
-pub type RfbBool = rfbBool;
+pub type RfbScreenInfoPtr = rfb::rfbScreenInfoPtr;
+pub type RfbBool = rfb::rfbBool;
 
 pub fn rfb_get_screen(width: i32, height: i32, bits_per_sample: i32, samples_per_pixel: i32, bytes_per_pixel: i32) -> RfbScreenInfoPtr {
-    let mut arg_len = 0 as i32;
-    let mut arg_ptr: *mut i8 = std::ptr::null_mut();
-
-    unsafe {
-        let server = rfbGetScreen(&mut arg_len, &mut arg_ptr, width, height, bits_per_sample, samples_per_pixel, bytes_per_pixel);
-        server
-    }
+    rfb::rfb_get_screen(width, height, bits_per_sample, samples_per_pixel, bytes_per_pixel)
 }
 
 pub fn rfb_screen_cleanup(ptr: RfbScreenInfoPtr) {
-    unsafe {
-        rfbScreenCleanup(ptr);
-    }
+    rfb::rfb_screen_cleanup(ptr);
 }
 
 pub fn rfb_framebuffer_malloc(ptr: RfbScreenInfoPtr, fb_size: u64) {
-    unsafe {
-        (*ptr).frameBuffer = malloc(fb_size as ::std::os::raw::c_ulong) as *mut i8;
-    }
+    rfb::rfb_framebuffer_malloc(ptr, fb_size);
 }
 
 pub fn rfb_framebuffer_free(ptr: RfbScreenInfoPtr) {
-    unsafe {
-        free((*ptr).frameBuffer as *mut ::std::os::raw::c_void);
-    }
+    rfb::rfb_framebuffer_free(ptr);
 }
 
 pub fn rfb_framebuffer_set_rgb16(ptr: RfbScreenInfoPtr, x: i32, y: i32, rgb16: u16) {
-	unsafe {
-        let addr = (*ptr).frameBuffer as *mut u16;
-        let fb_size = (*ptr).height * (*ptr).width * (*ptr).bitsPerPixel / 2;
-        let slice: &mut [u16] = slice::from_raw_parts_mut(addr, fb_size as usize);
-        let pos = (*ptr).width*y + x;
-        if pos < fb_size {
-            slice[pos as usize] = rgb16;
-        }
-	}
+    rfb::rfb_framebuffer_set_rgb16(ptr, x, y, rgb16);
 }
 
 pub fn rfb_init_server(ptr: RfbScreenInfoPtr) {
-    unsafe {
-        rfbInitServerWithPthreadsAndZRLE(ptr);
-    }
+    rfb::rfb_init_server(ptr);
 }
 
 pub fn rfb_run_event_loop(ptr: RfbScreenInfoPtr, usec: i64, run_in_background: RfbBool) {
-    unsafe {
-        rfbRunEventLoop(ptr, usec as ::std::os::raw::c_long, run_in_background);
-    };
+    rfb::rfb_run_event_loop(ptr, usec, run_in_background);
 }
---
+++ vncserver/src/rfb/mod.rs
@@ -0,0 +1,79 @@
+#![allow(dead_code)]
+#![allow(improper_ctypes)]
+#![allow(non_snake_case)]
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+
+//! # Libvncserver bindings
+//!
+//! This library contains safe Rust bindings for [Libvncserver](https://github.com/LibVNC/libvncserver)
+//!
+//! # Sample
+//!
+//! ```no_run
+//! use vncserver::*;
+//! fn main() {
+//!     let server = rfb_get_screen(400, 300, 8, 3, 4);
+//!     rfb_framebuffer_malloc(server, 400*300*4);
+//!     rfb_init_server(server);
+//!     rfb_run_event_loop(server, -1, 0);
+//! }
+//! ```
+
+use std;
+use core::slice;
+
+include!(concat!(std::env!("OUT_DIR"), "/rfb.rs"));
+
+
+pub fn rfb_get_screen(width: i32, height: i32, bits_per_sample: i32, samples_per_pixel: i32, bytes_per_pixel: i32) -> rfbScreenInfoPtr {
+    let mut arg_len = 0 as i32;
+    let mut arg_ptr: *mut i8 = std::ptr::null_mut();
+
+    unsafe {
+        let server = rfbGetScreen(&mut arg_len, &mut arg_ptr, width, height, bits_per_sample, samples_per_pixel, bytes_per_pixel);
+        server
+    }
+}
+
+pub fn rfb_screen_cleanup(ptr: rfbScreenInfoPtr) {
+    unsafe {
+        rfbScreenCleanup(ptr);
+    }
+}
+
+pub fn rfb_framebuffer_malloc(ptr: rfbScreenInfoPtr, fb_size: u64) {
+    unsafe {
+        (*ptr).frameBuffer = malloc(fb_size as ::std::os::raw::c_ulong) as *mut i8;
+    }
+}
+
+pub fn rfb_framebuffer_free(ptr: rfbScreenInfoPtr) {
+    unsafe {
+        free((*ptr).frameBuffer as *mut ::std::os::raw::c_void);
+    }
+}
+
+pub fn rfb_framebuffer_set_rgb16(ptr: rfbScreenInfoPtr, x: i32, y: i32, rgb16: u16) {
+	unsafe {
+        let addr = (*ptr).frameBuffer as *mut u16;
+        let fb_size = (*ptr).height * (*ptr).width * (*ptr).bitsPerPixel / 2;
+        let slice: &mut [u16] = slice::from_raw_parts_mut(addr, fb_size as usize);
+        let pos = (*ptr).width*y + x;
+        if pos < fb_size {
+            slice[pos as usize] = rgb16;
+        }
+	}
+}
+
+pub fn rfb_init_server(ptr: rfbScreenInfoPtr) {
+    unsafe {
+        rfbInitServerWithPthreadsAndZRLE(ptr);
+    }
+}
+
+pub fn rfb_run_event_loop(ptr: rfbScreenInfoPtr, usec: i64, run_in_background: rfbBool) {
+    unsafe {
+        rfbRunEventLoop(ptr, usec as ::std::os::raw::c_long, run_in_background);
+    };
+}
